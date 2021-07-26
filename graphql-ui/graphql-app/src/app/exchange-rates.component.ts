b732d79d5b8f4e5f8ccd5d60057e7f220de14141
Alexandre Jacquot <ajacquot@pictet-technologies.com>
2021-07-26 17:52:43 +0200

    Add working UI project with graphql

---
+++ graphql-ui/graphql-app/src/app/exchange-rates.component.ts
@@ -0,0 +1,45 @@
+import {Component, OnInit} from '@angular/core';
+import {Apollo, gql} from 'apollo-angular';
+
+@Component({
+  selector: 'exchange-rates',
+  template: `
+    <div *ngIf="loading">
+      Loading...
+    </div>
+    <div *ngIf="error">
+      Error :(
+    </div>
+    <div *ngIf="rates">
+      <div *ngFor="let rate of rates">
+        <p>{{ rate.currency }}: {{ rate.rate }}</p>
+      </div>
+    </div>
+  `,
+})
+export class ExchangeRates implements OnInit {
+  rates: any[];
+  loading = true;
+  error: any;
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+    this.apollo
+      .watchQuery({
+        query: gql`
+          {
+            rates(currency: "USD") {
+              currency
+              rate
+            }
+          }
+        `,
+      })
+      .valueChanges.subscribe((result: any) => {
+        this.rates = result?.data?.rates;
+        this.loading = result.loading;
+        this.error = result.error;
+      });
+  }
+}
